e5e77d886e71868cbc357172813252dc80c377da
Nikhil Bhardwaj <root@NikhilBhardwaj.in>
2012-07-10 13:36:07 +0530

    Problem 48 solved

---
+++ prob48/problem48.rb
@@ -0,0 +1,10 @@
+#!/usr/bin/env ruby
+
+sum = 0
+# Since we're only interested in the last 10 digits, we can use modular arithmetic
+modulator = (10 ** 10)
+(1..1000).each do |n|
+  sum += (n ** n) % modulator
+end
+
+puts sum.to_s[-10..-1]
