279988408e1a3099453a3476447befddaa431250
Nikhil Bhardwaj <root@NikhilBhardwaj.in>
2012-07-10 12:32:51 +0530

    Problem 30 soln added

---
+++ prob30/prob30.rb
@@ -0,0 +1,15 @@
+def sum_of_fifth_powers_of_digits(n)
+  sum = 0
+  while n > 0
+    rem = n % 10
+    sum = sum + (rem ** 5)
+    n = n / 10
+  end
+  return sum
+end
+
+$total = 0
+(10..(9**5)*9).each do |num|
+  $total += num if num == sum_of_fifth_powers_of_digits(num)
+end
+puts $total
